e3f81d59fd5e175f79722892a913a3bda961342f
SANGO AUDREY <115431019+sangus-alt@users.noreply.github.com>
2025-07-13 10:19:33 +0100

    Add files via upload

---
+++ HbrApplication.java
@@ -0,0 +1,12 @@
+// src/main/java/com/hbr/HbrApplication.java
+package com.hbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HbrApplication.class, args);
+    }
+}
